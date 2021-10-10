ab7325ec025798a0c1a9f402780b2f69954a8970
areille <areille@tuta.io>
2021-10-10 14:20:35 +0200

    Setup basic UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,90 @@
-<script>
-	export let name;
+<script lang="ts">
+  import { AceEditor } from "svelte-ace";
+  import "brace/mode/json";
+  import "brace/theme/chrome";
+
+  let input: string;
+  let className: string;
+  let result: string;
+
+  const sample = `{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+}    
+`;
+
+  const loadSample = () => {
+    className = "User";
+    input = sample;
+  };
+
+  const updateInput = (value) => {
+    input = value;
+  };
+
+  const convert = () => {
+    console.info("Converting...");
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>JSON to Dart class converter</h1>
+  <input type="text" placeholder="Class name" bind:value={className} />
+  <button on:click={loadSample}>Load sample</button>
+  <button id="convert-button" on:click={convert}>Convert</button>
+  <div class="flex">
+    <AceEditor
+      on:input={(obj) => updateInput(obj.detail)}
+      width="90%"
+      height="300px"
+      lang="json"
+      theme="chrome"
+      value={input}
+    />
+    <AceEditor
+      width="90%"
+      height="300px"
+      lang="json"
+      theme="chrome"
+      value={result}
+    />
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .flex {
+    display: flex;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  #convert-button {
+    z-index: 1;
+    position: relative;
+    font-size: inherit;
+    font-family: inherit;
+    color: white;
+    padding: 0.5em 1em;
+    outline: none;
+    border: none;
+    background-color: hsl(236, 32%, 26%);
+    overflow: hidden;
+    cursor: pointer;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  #convert-button::after {
+    content: "";
+    z-index: -1;
+    background-color: hsla(0, 0%, 100%, 0.2);
+    position: absolute;
+    top: -50%;
+    bottom: -50%;
+    width: 1.25em;
+    transform: translate3d(-525%, 0, 0) rotate(35deg);
+  }
+
+  #convert-button:hover::after {
+    transition: transform 0.45s ease-in-out;
+    transform: translate3d(200%, 0, 0) rotate(35deg);
+  }
 </style>
