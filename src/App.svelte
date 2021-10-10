fc75a6c14dc17aa4d1fc785a1fa23d3a348791ec
areille <areille@tuta.io>
2021-10-10 17:31:45 +0200

    Convert more complex jsons

--- src/App.svelte
+++ src/App.svelte
@@ -12,47 +12,47 @@
   let className: string;
   let result: string;
 
-  const sample = `{
-  	"userId": 1,
-  	"id": 1,
-  	"title": "delectus aut autem",
-  	"completed": false
-  }
-  `;
-
-  const loadSample = () => {
-    className = "Todo";
-    input = sample;
-  };
-
   //   const sample = `{
-  //   "id": 1,
-  //   "name": "Leanne Graham",
-  //   "username": "Bret",
-  //   "email": "Sincere@april.biz",
-  //   "address": {
-  //     "street": "Kulas Light",
-  //     "suite": "Apt. 556",
-  //     "city": "Gwenborough",
-  //     "zipcode": "92998-3874",
-  //     "geo": {
-  //       "lat": "-37.3159",
-  //       "lng": "81.1496"
-  //     }
-  //   },
-  //   "phone": "1-770-736-8031 x56442",
-  //   "website": "hildegard.org",
-  //   "company": {
-  //     "name": "Romaguera-Crona",
-  //     "catchPhrase": "Multi-layered client-server neural-net",
-  //     "bs": "harness real-time e-markets"
-  //   }
-  // }`;
+  //   	"userId": 1,
+  //   	"id": 1,
+  //   	"title": "delectus aut autem",
+  //   	"completed": false
+  // }
+  //   `;
 
   //   const loadSample = () => {
-  //     className = "User";
+  //     className = "Todo";
   //     input = sample;
   //   };
+
+  const sample = `{
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+}`;
+
+  const loadSample = () => {
+    className = "User";
+    input = sample;
+  };
 
   const formatJson = () => {
     input = JSON.stringify(JSON.parse(input), null, "\t");
--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -5,21 +5,75 @@ interface JsonMap {
 }
 interface JsonArray extends Array<AnyJson> {}
 
-function tsTypeToDartType(tsType: string, value?: number): string {
-  switch (tsType) {
-    case "string":
-      return "String";
-    case "boolean":
-      return "bool";
-    case "number":
-      if (value % 1 === 0) {
-        return "int";
-      } else {
-        return "double";
-      }
-    default:
-      return "unknown";
+interface DartClass {
+  name: string;
+  variables: {
+    name: string;
+    type: string;
+  }[];
+}
+
+function extractClasses(json: AnyJson, className: string): DartClass[] {
+  let classes = Array<DartClass>();
+  let newClass: DartClass = {
+    name: className,
+    variables: [],
+  };
+  Object.keys(json).forEach((key) => {
+    let name: string, type: string;
+    name = key;
+    switch (typeof json[key]) {
+      case "string":
+        type = "String";
+        break;
+      case "boolean":
+        type = "bool";
+        break;
+      case "number":
+        if (<number>json[key] % 1 === 0) {
+          type = "int";
+          break;
+        } else {
+          type = "double";
+          break;
+        }
+      default:
+        // Case complex type
+        const className = key.charAt(0).toUpperCase() + key.slice(1);
+        type = key.charAt(0).toUpperCase() + key.slice(1);
+        classes = [...extractClasses(json[key], className), ...classes];
+        break;
+    }
+    newClass.variables.push({ name, type });
+  });
+  classes.push(newClass);
+  return classes;
+}
+
+function writeClass(dClass: DartClass): string {
+  return `class ${dClass.name} {
+${dClass.variables.map((v) => `  final ${v.type} ${v.name};`).join("\n")}
+            
+  const ${dClass.name}({
+${dClass.variables.map((v) => `    required this.${v.name};`).join("\n")}
+  });
+          
+  factory ${dClass.name}.fromJson(Map<String, dynamic> json) {
+    return ${dClass.name}(
+${dClass.variables
+  .map((v) => `      ${v.name}: json['${v.name}'] as ${v.type},`)
+  .join("\n")}
+    );
   }
+          
+  Map<String, dynamic> toJson() {
+    return {
+${dClass.variables.map((v) => `      '${v.name}': this.${v.name},`).join("\n")}
+    };
+  }
+}
+          
+`;
 }
 
 export function convert2Dart(
@@ -27,37 +81,13 @@ export function convert2Dart(
   className: string = "Generated"
 ): string {
   const jsonObj: AnyJson = JSON.parse(rawInput);
-  let elements = Array<{ variable: string; type: string }>();
-  Object.keys(jsonObj).forEach((key, i) => {
-    const elem = {
-      variable: key,
-      type: tsTypeToDartType(
-        typeof jsonObj[key],
-        typeof jsonObj[key] === "number" ? jsonObj[key] : null
-      ),
-    };
-    elements.push(elem);
-  });
+  const classes = extractClasses(jsonObj, className);
+  console.log(classes);
+  let result = "";
 
-  return `class ${className} {
-${elements.map((e) => `  final ${e.type} ${e.variable};`).join("\n")}
-  
-  const ${className}({
-${elements.map((e) => `    required this.${e.variable};`).join("\n")}
-  });
-
-  factory ${className}.fromJson(Map<String, dynamic> json) {
-    return ${className}(
-${elements
-  .map((e) => `      ${e.variable}: json['${e.variable}'] as ${e.type},`)
-  .join("\n")}
-    );
+  for (let i = 0; i < classes.length; i++) {
+    result += writeClass(classes[i]);
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-${elements.map((e) => `      '${e.variable}': this.${e.variable},`).join("\n")}
-    };
-  }
-}`;
+  return result;
 }
